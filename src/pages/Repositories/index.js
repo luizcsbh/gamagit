8f8dd2f2069be955129a83799309a41799e8f94b
Luiz Santos <luizcsbh@gmail.com>
2020-05-28 17:44:51 -0300

    alterando projeto

---
+++ src/pages/Repositories/index.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState} from 'react';
+import * as S from './styled';
+
+export default function Repositories(){
+    const [ repositories, setRepositories] = useState([]);
+    
+    useEffect(() => {
+        let repositoriesName = localStorage.getItem('respositoriesName');
+        repositoriesName = JSON.parse(repositoriesName);
+        setRepositories(repositoriesName);
+        localStorage.clear();
+    }, []);
+
+    return (
+        <S.Container>
+            <S.Title>Repositórios</S.Title>
+            <S.List>
+                { repositories.map(repository => {
+                    return (
+                        <S.ListItem>Repositórios: { repository }</S.ListItem>
+                    )
+                })}
+            </S.List>
+        </S.Container>
+    )
+
+}
